831d1aa63903b8cde3be7a84be2aeb15e650a4f9
n0rig <josh.giron@solarwinds.com>
2019-09-17 23:16:23 -0400

    Fix kubectl command context information (copy pasta from terraform command's file)

--- toolbox-cli/src/commands/bitnami/kubectl/1.15.3.ts
+++ toolbox-cli/src/commands/bitnami/kubectl/1.15.3.ts
@@ -1,7 +1,7 @@
 import {Command, flags} from '@oclif/command'
 import {execute_docker, get_command_version} from '../../../helpers'
 
-export default class RunTerraform extends Command {
+export default class RunKubectl extends Command {
   // Good tutorial on what we are doing here:
   //  https://blog.heroku.com/cli-flags-get-started-with-oclif
   //  https://zaiste.net/nodejs-child-process-spawn-exec-fork-async-await/
@@ -9,12 +9,12 @@ export default class RunTerraform extends Command {
   static strict = false // Allow passing in sub-args to the docker container
   //static hidden = true // Prevent the oclif from showing it's help
 
-  static description = `Launch Terraform ${get_command_version(__filename)} via an executable docker container.`
+  static description = `Launch kubectl ${get_command_version(__filename)} via an executable docker container.`
 
   static examples = [
-    `$ toolbox run:terraform refresh`,
-    `$ toolbox run:terraform plan`,
-    `$ toolbox run:terraform apply`,
+    `$ kubectl get pods -A`,
+    `$ kubectl get nodes -A`,
+    `$ kubectl apply -k .`,
   ]
 
   async run() {
